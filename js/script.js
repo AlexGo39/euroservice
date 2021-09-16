8ea9624857f076f7524f845000e688f3ab9290bf
Alexey <alex.gt.sa@gmail.com>
2021-09-16 13:59:22 +0200

    sliders\main slider

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+ $('.main_slider').slick({
+    arrows:true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    easing: "easeInQuad",
+    autoplay: false,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    draggable: false,
+ }
+ );
+});
