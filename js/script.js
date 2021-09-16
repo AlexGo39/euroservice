e2e3c26093751683485e4c2db8644d40993c7fba
Alexey <alex.gt.sa@gmail.com>
2021-09-16 16:43:43 +0200

    partners slider

--- js/script.js
+++ js/script.js
@@ -12,4 +12,20 @@ $(document).ready(function(){
     draggable: false,
  }
  );
+
+
+ $('.partners_slider').slick({
+    arrows: true,
+    speed: 1000,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    infinite: true,
+    initialSlide: 0,
+    easing: "easeInQuad",
+    autoplay: false,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    draggable: true,
+ }
+ );
 });
